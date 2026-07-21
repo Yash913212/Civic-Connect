894f22ded907c78006ef54620a09b7b7ed712dbc
Yash <yash@example.com>
2026-07-21 22:44:53 +0530

    Default backend DB to Neon

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "CivicConnect"
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/civic"
+    DATABASE_URL: str = "postgresql://neondb_owner:npg_vf3YbaB5eESi@ep-flat-king-atz28uzi-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
